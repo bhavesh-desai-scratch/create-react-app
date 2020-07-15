26add3046d0dbf0aeb750d9416af9aafa76fd18f
BHAVESH <bhavesh@weareadaptive.com>
2020-07-15 15:00:04 +0100

    add service mocking

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -738,6 +738,14 @@ module.exports = function (webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new webpack.NormalModuleReplacementPlugin(/\.*service.ts/, function (
+        resource
+      ) {
+        resource.request = resource.request.replace(
+          /\.*service.ts/,
+          '.mock.ts'
+        );
+      }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
